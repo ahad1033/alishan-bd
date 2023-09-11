65baa1752a737c96d37c6dcbf99273fe944e0ea7
Ahad Ahamed <a.ahad2232@gmail.com>
2023-09-11 14:24:53 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Main from './Layout/Main'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='max-w-screen-xl mx-auto'>
+      <Main />
+    </div>
   </React.StrictMode>,
 )
