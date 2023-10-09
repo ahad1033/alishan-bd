ec3bd9c57a5df58936e002f09bdb5129a723968a
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-09 12:52:56 +0600

    banner section added

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <h2 className='text-center font-bold'>banner is here</h2>
+        </div>
+    );
+};
+
+export default Banner;
