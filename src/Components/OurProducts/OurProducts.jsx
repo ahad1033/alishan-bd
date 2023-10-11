49ba283d968908e917a405335ede6052a89ee395
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-11 18:01:41 +0600

    products section modified

--- src/Components/OurProducts/OurProducts.jsx
+++ src/Components/OurProducts/OurProducts.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const OurProducts = () => {
     return (
-        <section>
-            <h2 className='text-center text[var(--text-primary)]'>Our Stone Basins</h2>
-            <div>
-
+        <section className='bg-base-200'>
+            <h2 className='text-center text-black font-bold text-5xl py-8'>Our Stone Basins</h2>
+            <div className='section-container'>
+                
             </div>
         </section>
     );
