6d00063077406c9ca57770bbb7f75d7fd12cafdc
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-14 13:06:19 +0600

    code modified and bug fixes

--- src/Components/OurProducts/SingleProducts.jsx
+++ src/Components/OurProducts/SingleProducts.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 const SingleProducts = ({ product }) => {
 
-    const { img, basinSize, mirrorSize,topSize, price, color, style, weight, modelNo  } = product;
+    const { img, basinSize, mirrorSize, topSize, price, color, style, weight, modelNo } = product;
 
     console.log(product);
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
             <figure><img src={img.img1} alt="products" /></figure>
             <div className="card-body">
-                <h2 className="card-title text-[var(--text-primary)] font-bold justify-center">Model: {modelNo}</h2>
+                <h2 className="card-title text-[var(--text-primary)] font-bold text-2xl justify-center">Model: {modelNo}</h2>
                 <p>Style: {style}</p>
                 <p>Color: {color}</p>
                 <p>Basin Size: {basinSize}</p>
                 <p>Mirror Size: {mirrorSize}</p>
+                <p>Top Size: {topSize}</p>
+                <p>Weight: {weight}</p>
                 <div className="card-actions justify-center mt-2">
                     <button className="btn w-fit bg-[var(--text-primary)] border-0  text-white font-semibold">Add to Cart</button>
                 </div>
--- src/Components/SubscribeSection/SubscribeSection.jsx
+++ src/Components/SubscribeSection/SubscribeSection.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SubscribeSection = () => {
     return (
         <div className='section-container py-11 flex gap-20justify-between bg-black'>
-            <div>
+            <div className='w-1/3'>
                 <h1 className='mb-4 text-5xl text-white font-bold'>Alishan</h1>
                 <p className='mb-5 text-white  text-xl'>Register here to get daily updates.</p>
                 <div className="form-control">
@@ -14,13 +14,13 @@ const SubscribeSection = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex text-white gap-10 items-center mx-auto justify-end'>
-                <div className='flex gap-10'>
+            <div className='flex w-2/3 text-white gap-10 items-center mx-auto'>
+                <div className='flex justify-between w-full  gap-10'>
                     <div>
                         <h3 className='font-semibold my-7 uppercase'>Contact Us</h3>
                         <h3 className='font-semibold my-7 uppercase'>Partners</h3>
                     </div>
-                    <div>
+                    <div className='text-center'>
                         <h3 className='font-semibold my-7 uppercase'>Advertise</h3>
                         <h3 className='font-semibold my-7 uppercase'>Times of Service</h3>
                     </div>
