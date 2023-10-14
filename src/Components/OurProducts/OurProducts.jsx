c689e5ed22c20d1abe0caefd650730aa21889f45
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-14 11:41:45 +0600

    products data created and fetched to the single product

--- src/Components/OurProducts/OurProducts.jsx
+++ src/Components/OurProducts/OurProducts.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SingleProducts from './SingleProducts';
 
 const OurProducts = () => {
+
+    const [ products, setProducts] = useState([]);
+    
+    useEffect( () =>{
+        fetch("products.json")
+        .then(res => res.json())
+        .then(data => setProducts(data))
+        .catch(err => console.log(err))
+    },[])
+
     return (
         <section className='bg-base-200'>
             <h2 className='text-center text-black font-bold text-5xl py-8'>Our Stone Basins</h2>
             <div className='section-container'>
-                
+                {products.map((product) => (<SingleProducts key={product.modelNo} product={product} />))}
             </div>
         </section>
     );
---
+++ src/Components/OurProducts/SingleProducts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SingleProducts = ({ product }) => {
+
+    console.log(product);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SingleProducts;
