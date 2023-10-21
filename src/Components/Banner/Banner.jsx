47bf0073aabc03cf9a9802c1da66b8cacbb1588b
Ahad Ahamed Akash <118015905+ahad1033@users.noreply.github.com>
2023-10-21 12:41:11 +0600

    UI modified

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,9 +4,9 @@ const Banner = () => {
     return (
         <div className='bg-cover bg-center bg-no-repeat h-screen' style={{ backgroundImage: `url(https://i.ibb.co/1ftZp94/C.jpg)` }}>
             <div className='section-container flex flex-col justify-center bg-cover h-screen bg-opacity-30 bg-black'>
-                <h1 className='font-bold text-6xl text-[#FCD462] '>Experience <p className='mt-5'>the Elegance of</p><p className='mt-5'>Nature-Inspired Luxury.</p></h1>
-                <p className='my-5 text-[#FCD462] text-2xl'>Stones Beauty - That Makes Your Home Luxurious.</p>
-                <button className='btn w-fit bg-[var(--text-primary)] border-0  text-white font-semibold'>Explore More</button>
+                <h1 className='font-semibold md:font-bold lg:font-bold text-5xl md:text-4xl lg:text-6xl text-[#FCD462] ps-3'>Experience <p className='mt-5'>the Elegance of</p><p className='mt-5'>Nature-Inspired Luxury.</p></h1>
+                <p className='ps-3 my-5 text-[#FCD462] text-2xl'>Stones Beauty - That Makes Your Home Luxurious.</p>
+                <button className='ms-3 btn w-fit bg-[var(--text-primary)] border-0 text-white font-semibold'>Explore More</button>
             </div>
         </div>
     );
--- src/Components/SubscribeSection/SubscribeSection.jsx
+++ src/Components/SubscribeSection/SubscribeSection.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const SubscribeSection = () => {
     return (
-        <div className='section-container py-11 flex gap-20justify-between bg-black'>
-            <div className='w-1/3'>
+        <div className='section-container py-11 flex flex-col lg:flex-row md:flex-row gap-20 justify-between bg-black'>
+            <div className='flex flex-col sm:w-full md:w-1/3 lg:w-1/3 items-center md:items-start lg:items-start gap-5'>
                 <h1 className='mb-4 text-5xl text-white font-bold'>Alishan</h1>
-                <p className='mb-5 text-white  text-xl'>Register here to get daily updates.</p>
+                <p className='mb-5 text-white text-lg'>Register here to get daily updates.</p>
                 <div className="form-control">
                     <div className="input-group">
                         <input type="text" placeholder="Enter your email" className="input input-bordered" />
@@ -14,8 +14,8 @@ const SubscribeSection = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex w-2/3 text-white gap-10 items-center mx-auto'>
-                <div className='flex justify-between w-full  gap-10'>
+            <div className='flex sm:w-full md:w-2/3 lg:w-2/3 px-5 text-white items-center mx-auto'>
+                <div className='flex justify-between w-full gap-3 text-xs md:text-sm lg:text-sm'>
                     <div>
                         <h3 className='font-semibold my-7 uppercase'>Contact Us</h3>
                         <h3 className='font-semibold my-7 uppercase'>Partners</h3>
